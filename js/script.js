a99d8cac7651408e1727938e397252d99115ecc4
Eduard <eduardcojocaru2016@gmail.com>
2025-10-23 20:53:59 +0100

    Added lots of game stuff

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
+// Importing the Card class
+import {Card} from './card.mjs';
+
+// Using the buildPack method to create a pack of cards
+const cards = new Card();
+cards.buildPack();
+
 // Declaring screen width and height constants
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
-
-const suits = ["hearts", "diamonds", "clubs", "spades"];
-const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-const pack = [];
-
-function buildPack() {
-    for (let suit of suits){
-        for (let rank of ranks) {
-            pack.push({
-                suit: suit,
-                rank: rank,
-                img: `${rank}_of_${suit}.png`
-            })
-        }
-    }
-}
-
-buildPack();
 
 // Declaring fall time constant
 const fallTime = 10000;
@@ -26,8 +15,8 @@ const fallTime = 10000;
 // Function to create a falling card effect
 function createFallingCard() {
     // Declaring image path constant
-    const index = Math.floor(Math.random() * pack.length)
-    const image = `../images/cards/${pack[index].img}`;
+    const index = Math.floor(Math.random() * cards.pack.length)
+    const image = `../images/cards/${cards.pack[index].img}`;
     
     // Declaring a constant to make the card into an element
     const card = document.createElement("img");
@@ -43,7 +32,7 @@ function createFallingCard() {
     // Adding the card to the page
     document.body.appendChild(card);
 
-    // Arrow function to make sure the card is deleted after 10000ms
+    // Card is deleted after 10000ms
     setTimeout(() => {document.body.removeChild(card)}, fallTime);
 }
 
