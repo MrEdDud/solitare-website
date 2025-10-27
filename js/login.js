b3b4af0d054f398c259abc1fda6e131e6bdef32d
Eduard <eduardcojocaru2016@gmail.com>
2025-10-27 12:13:17 +0000

    Displaying all the table cards

---
+++ js/login.js
@@ -0,0 +1,111 @@
+// Regex patterns for password, email, and phone validation
+const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+const emailRegex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+const phoneRegex = new RegExp("^07\\d{8,9}$");
+
+// ADD SCORE
+// Function to register a new user
+function registerUser(){ 
+    // Variables to check if the inputs are valid
+    let nameCheck = false;
+    let passwordCheck = false;
+    let emailCheck = false;
+    let phoneCheck = false;
+
+    // Get the values from the inputs
+    const register_name = document.getElementById("registerName").value;
+    const register_password = document.getElementById("registerPassword").value;
+    const register_email = document.getElementById("registerEmail").value;
+    const register_phone = document.getElementById("registerPhone").value;
+    const user_score = 0;
+
+    // An object to hold the registration data
+    const registerData = {
+        name: register_name,
+        password: register_password,
+        email: register_email,
+        phone: register_phone,
+        score: user_score,
+    }
+    
+    // Checking if the name is valid
+    if(register_name.length >= 3 && register_name.length <= 20) {
+        if (localStorage.getItem(register_name) !== null){ 
+            showError("errorName", "Name already taken!");
+        }
+        else {
+            nameCheck = true;
+            hideError("errorName");
+        }
+    } else {
+        showError("errorName", "Name must be 3-20 characters long");
+    }
+
+    // Checking if the password is valid
+    if(strongRegex.test(register_password)) {
+        passwordCheck = true;
+        hideError("errorPassword");
+    } else {
+        showError("errorPassword", "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.");
+    };
+
+    // Checking if the email is valid
+    if(emailRegex.test(register_email)) {
+        emailCheck = true;
+        hideError("errorEmail");
+    } else {
+        showError("errorEmail", "Invalid email address!");
+    }
+
+    // Checking if the phone number is valid
+    if(phoneRegex.test(register_phone)) {
+        phoneCheck = true;
+        hideError("errorPhone");
+    } else {
+        showError("errorPhone", "Invalid phone number! It should start with 07 and be 10-11 digits long.");
+    }
+
+    // Checking if all inputs are valid
+    if (nameCheck && passwordCheck && emailCheck && phoneCheck) {
+        // Converting the users data into JSON and storing it in local storage
+        const registerDataStr = JSON.stringify(registerData);
+        localStorage[registerData.name] = registerDataStr;
+        // Changes window after 1 second
+        setTimeout(() => {window.location.href = "index.html"}, 1000);
+    }
+}
+
+// Helper functions to show and hide error messages
+function showError(id, message) {
+    const elementID = document.getElementById(id);
+    elementID.innerHTML = message;
+    elementID.classList.remove("hidden");
+}
+function hideError(id) {
+    const elementID = document.getElementById(id);
+    elementID.innerHTML = "";
+    elementID.classList.add("hidden");
+}
+
+// Function to log the user in
+function loginUser(){
+    // Get the values from the inputs
+    const login_name = document.getElementById("loginName").value;
+    const login_password = document.getElementById("loginPassword").value;
+
+    if(localStorage[login_name] === undefined) {
+        console.log("User not found");
+        showError("errorLoginName", "User not found!");
+    } else {
+        hideError("errorLoginName");
+        const userObj = JSON.parse(localStorage[login_name]);
+        
+        if (login_password === userObj.password) {
+            hideError("errorLoginPassword");
+            sessionStorage.setItem("loggedInUser", login_name);
+            setTimeout(() => {window.location.href = "game.html"}, 1000);
+        } else {
+            showError("errorLoginPassword", "Incorrect password!");
+        }
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Importing the Card class
+import {Card} from './card.mjs';
+
+// Using the buildPack method to create a pack of cards
+const cards = new Card();
+cards.buildPack();
+
+// Declaring screen width and height constants
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+// Declaring fall time constant
+const fallTime = 10000;
+
+// Function to create a falling card effect
+function createFallingCard() {
+    // Declaring image path constant
+    const index = Math.floor(Math.random() * cards.pack.length)
+    const image = `../images/cards/${cards.pack[index].img}`;
+    
+    // Declaring a constant to make the card into an element
+    const card = document.createElement("img");
+
+    // Setting the cards source image and class name for styling
+    card.src = image;
+    card.className = "falling-card";
+
+    // Setting a random x position for the card and a set y position above the screen for the card
+    card.style.left = Math.random() * screenWidth + "px";
+    card.style.top = -128 + "px";
+
+    // Adding the card to the page
+    document.body.appendChild(card);
+
+    // Card is deleted after 10000ms
+    setTimeout(() => {document.body.removeChild(card)}, fallTime);
+}
+
+// Set interval to create falling cards every 500ms
+setInterval(createFallingCard, 500);
